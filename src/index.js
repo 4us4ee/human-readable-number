0b0448388ad7049e88492af015575734bc98c56e
Vika Usova <getsbistorm1@mail.ru>
2024-08-16 22:31:22 +0700

    docs: index.html done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function toReadable (number) {
-  
+ module.exports =
+function toReadable (number) {
+   let numToStr = String(number);
+   let n=['zero','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+   let t=['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+   let h='hundred';
+   if(numToStr.length==1) return n[number];
+   if(numToStr.length==2 && number<=19) return n[number];
+   if(numToStr.length==2 && number>19){
+    if(number%10==0){
+        return t[Math.floor(number/10)-2];
+    }
+    else {
+        return   t[Math.floor(number/10)-2] + ' ' + n[number%10];
+     
+    }
+} 
+    if(numToStr.length==3) 
+        if(Math.floor((number%100))<=19 && Math.floor((number%100))>0){
+            return n[Math.floor(number/100)] +' ' +h + ' ' + n[Math.floor((number%100))];
+        }
+        if(Math.floor((number%100)/10)==0 && !(Math.floor((number%100)%10)==0)){
+           return n[Math.floor(number/100)] +' ' +h + ' ' + n[Math.floor((number%100)%10)];
+        }
+        else if(Math.floor((number%100)%10)==0 && !(Math.floor((number%100)/10)==0)){
+          return  n[Math.floor(number/100)] + ' ' + h + ' ' + t[Math.floor((number%100)/10)-2] ;
+        }
+        else if((Math.floor((number%100)%10)==0) && (Math.floor((number%100)/10)==0)){
+            return n[Math.floor(number/100)] +' ' + h ;
+        }
+        else{
+
+            return n[Math.floor(number/100)] + ' ' + h + ' '  + t[Math.floor((number%100)/10)-2] + ' ' + n[Math.floor((number%100)%10)];
+        }
 }
